085d8b657f8286912d3c2bbc35365eeca229a909
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-12-04 23:19:10 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+const allProducts = ["Samsung", "IPhone", "Pixel"]
 
 const App = () => {
+  const [cart, setCart] = useState([])
+  function handleAddToCart(item){
+    setCart([...cart, item])
+  }
+  function handleDelete(i){
+    setCart(cart.filter((item,ind)=>ind!=i))
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h3>List of Products</h3>
+      {
+        allProducts.map(item => (
+          <div>
+            <p>{item}</p>
+            <button onClick={() => handleAddToCart(item)} >Add</button>
+          </div>
+        ))
+      }
+      <h3>Cart</h3>
+      {
+        cart.length === 0 ? (
+          <p>There is no items in the cart</p>
+        )
+          :
+          (
+            cart.map((item,i) => (
+              <div>
+                <p>{item}</p>
+                <button onClick={() => handleDelete(i)} >Remove</button>
+              </div>
+            ))
+          )
+      }
     </div>
   )
 }
